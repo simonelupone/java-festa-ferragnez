3e109fb1d3247ee8ac70f52e84e7e5addade66fe
simonelupone <simone.lupone@gmail.com>
2025-07-16 16:01:56 +0200

    feat: implement CheckGuest functionality to verify guest access with for loop

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,31 @@
 package com.ferragnez.party;
 
-public class CheckGuest {
+import java.util.Scanner;
 
+public class CheckGuest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] guests = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
+
+        System.out.println("Inserisci il tuo nome!");
+        String guestName = sc.nextLine().toLowerCase().strip();
+
+        boolean isGuest = false;
+
+        // FOR
+        for (int i = 0; i < guests.length; i++) {
+
+            if (guestName.equals(guests[i].toLowerCase())) {
+                isGuest = true;
+                break;
+            }
+        }
+
+        String response = isGuest ? "Accesso consentito!" : "Accesso negato!";
+        System.out.println(response);
+
+        sc.close();
+    }
 }
